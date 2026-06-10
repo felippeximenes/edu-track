658c95515959e21ecee5cd973a4aa670dee9a2f4
felippeximenes <felippelpximenes@outlook.com>
2026-06-10 11:22:33 -0300

    fix: suppress useEffect missing dependency ESLint warning in InstructorPanel

--- frontend/src/pages/InstructorPanel.tsx
+++ frontend/src/pages/InstructorPanel.tsx
@@ -37,6 +37,7 @@ export default function InstructorPanel() {
       .finally(() => setLoading(false));
   }
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { loadCourses(); }, []);
 
   function openModal(mode: ModalMode, courseId?: number) {
